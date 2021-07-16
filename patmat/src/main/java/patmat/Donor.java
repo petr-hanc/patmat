a401eb63d95962c8ede4b2a01bad1275ce81a554
petr-hanc <hancp@centrum.cz>
2021-07-16 21:07:45 +0200

    Change from JPA to JDBC - start

--- patmat/src/main/java/patmat/Donor.java
+++ patmat/src/main/java/patmat/Donor.java
@@ -1,44 +1,22 @@
 package patmat;
 
+import java.time.LocalDate;
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
-//import javax.persistence.Transient;
-import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-// import javax.validation.constraints.NotBlank;
-
 
 import lombok.Data;
 
 @Data
-@Entity
-@Table(name = "donors")
 public class Donor {
-
-    @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    @Column(name = "id")
 	private long id;
-    @Column(name = "firstname")
+    private LocalDate createdOn;	
 	private String firstName;
     @NotNull
     @Size(min=1, message="Příjmení musí mít aspoň 1 znak")
-    @Column(name = "lastname")
 	private String lastName;
-    @Column(name = "town")
 	private String town;
-    @OneToMany /* (cascade = CascadeType.ALL, orphanRemoval = true) */
-    @JoinColumn(name = "donor_id")
 	private List<Donation> donations;
 
 	public Donor() {
--- patmat/src/main/java/patmat/DonorRepository.java
+++ patmat/src/main/java/patmat/DonorRepository.java
@@ -3,7 +3,7 @@ package patmat;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-//import java.util.List;
+
 
 @Repository
 public interface DonorRepository extends JpaRepository<Donor, Long> {
