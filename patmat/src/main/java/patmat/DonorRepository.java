3ca29a4e620267f115bb8aab66162210b739978d
petr-hanc <hancp@centrum.cz>
2021-07-30 11:51:13 +0200

    list methods in DonorRepository interface

--- patmat/src/main/java/patmat/DonorRepository.java
+++ patmat/src/main/java/patmat/DonorRepository.java
@@ -1,10 +1,14 @@
 package patmat;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DonorRepository {
     List<Donor> findAll();
+    Optional<Donor> findById(long donorId);
+	Donor save(Donor donor);
+	void delete(Donor donor);
 }
