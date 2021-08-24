7497ed30b7f2cee0548af93a575bb53e166b2fda
petr-hanc <hancp@centrum.cz>
2021-08-25 00:25:58 +0200

    remove unused imports

--- patmat/src/main/java/patmat/Donor.java
+++ patmat/src/main/java/patmat/Donor.java
@@ -2,21 +2,18 @@ package patmat;
 
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 //import javax.persistence.Transient;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 // import javax.validation.constraints.NotBlank;
-
 
 import lombok.Data;
 
